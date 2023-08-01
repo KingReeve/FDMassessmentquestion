0a5fb29ae8579827f14bf0a8b275cffa821048c9
KingReeve <140770868+KingReeve@users.noreply.github.com>
2023-07-31 20:48:05 -0500

    Create ChangeWord.py

---
+++ ChangeWord.py
@@ -0,0 +1,17 @@
+# take an input string of a single word, return a string wherein every word of the input string is either 'x' if that letter only appeared once, or 'y' if it appeared more than once.
+
+Word = str(input('Input a word'))
+
+def ChangeWord(word):
+
+    Dictionary = dict((letter,word.count(letter)) for letter in set(word))
+    #characters = list(word)
+    newword = str()
+    for letter in word:
+        if Dictionary[letter] == 1:
+            newword += 'x'
+        else:
+            newword += 'y'
+    return newword
+
+print(ChangeWord(Word))
